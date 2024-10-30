392a3ea5fe395329bbebf38645f4e3f38fb2914b
triafnirezkylestari <triafnirezkylestari@gmail.com>
2024-10-30 12:08:21 +0700

    initial commit 3

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>triafni rezky lestari</h1>
+    <h2>607012300003</h2>
+</body>
+</html>
